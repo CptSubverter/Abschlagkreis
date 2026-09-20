fbfa041040fd06581fb72c4d6b7e8a7b18aa7a18
CptSubverter <michael.hoever@icloud.com>
2026-09-20 22:23:55 +0200

    App-Cache aktualisieren damit neue Speicherfunktion geladen wird

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE="abschlagkreis-v1";
-const ASSETS=["./","./index.html","./kreis.svg","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+const CACHE="abschlagkreis-v2";
+const ASSETS=["./","./index.html","./kreis.svg","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=="abschlagkreis-v2").map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+  e.respondWith(fetch(e.request).then(res=>{
     const copy=res.clone();
     caches.open(CACHE).then(c=>c.put(e.request,copy));
     return res;
-  })));
+  }).catch(()=>caches.match(e.request)));
 });
