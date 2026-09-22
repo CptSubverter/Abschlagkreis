bb415a692280197bb6dc12bf5b9aee0aabc4b1a5
CptSubverter <michael.hoever@icloud.com>
2026-09-22 07:11:01 +0200

    Service Worker Cache auf v35 aktualisieren

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE="abschlagkreis-v34";
+const CACHE="abschlagkreis-v35";
 const ASSETS=["./","./index.html","./kreis.svg","./manifest.json","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=="abschlagkreis-v34").map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=="abschlagkreis-v35").map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   e.respondWith(fetch(e.request).then(res=>{
